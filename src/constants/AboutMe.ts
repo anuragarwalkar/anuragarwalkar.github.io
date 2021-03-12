0c1697dee3cd3ca6b19d1771c6e07d5f5b0bb4f1
Anurag <anuragarwalkar@gmail.com>
2021-03-12 14:01:48 +0530

    refactoring made constants

---
+++ src/constants/AboutMe.ts
@@ -0,0 +1,50 @@
+export const Links = [
+  {
+    key: 1,
+    className: "about__info__social--youtube",
+    href: "https://www.youtube.com/anuragarwalkar",
+    icon: "youtube",
+  },
+  {
+    key: 2,
+    className: "about__info__social--youtube",
+    href: "https://bit.ly/anuragdev",
+    icon: "youtube",
+  },
+  {
+    key: 3,
+    className: "about__info__social--instagram",
+    href: "https://www.instagram.com/anuragarwalkar",
+    icon: "instagram",
+  },
+  {
+    key: 4,
+    className: "about__info__social--twitter",
+    href: "https://twitter.com/anruagarwalkar",
+    icon: "twitter",
+  },
+  {
+    key: 5,
+    className: "about__info__social--github",
+    href: "https://github.com/anuragarwalkar",
+    icon: "github",
+  },
+  {
+    key: 6,
+    className: "about__info__social--medium",
+    href: "https://anuragarwalkar.medium.com",
+    icon: "medium",
+  },
+  {
+    key: 7,
+    className: "about__info__social--stack-overflow",
+    href: "https://stackoverflow.com/users/10028375/anurag-arwalkar",
+    icon: "stack-overflow",
+  },
+];
+
+export const Description = `I Anurag Self-motivated IT professional with good knowledge and
+proficiency in HTML, CSS, JavaScript. Expertise in frameworks like
+React.js, Angular, Node.js and great knowledge of No SQL database like
+MongoDb as well as strong skills and ability in writing clean and
+efficient code. check out my work on GitHub.`;
