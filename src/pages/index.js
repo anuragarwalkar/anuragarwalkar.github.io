60b773f19f9b676cf8dea03bf7ce549a2510b2f5
anuragarwalkar <anuragarwalkar@ehrdata.com>
2021-12-10 20:13:30 +0530

    initial changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
+import "css/aos.css?ver=1.1.0";
+import "css/bootstrap.min.css?ver=1.1.0";
+import "css/main.css?ver=1.1.0";
+import "https://fonts.googleapis.com/css?family=Montserrat:400,700,200";
+import "https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css";
 import React from "react";
 
 // markup
 const IndexPage = () => {
   return (
     <main>
-      <title>Home Page</title>
+      <title>Anurag Arwalkar Portfolio</title>
       <h1>Hello world...😎</h1>
     </main>
   );
