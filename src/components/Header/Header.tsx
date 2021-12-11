b4e15a9937b7619e7243087c36d362d9d8d30517
anuragarwalkar <anuragarwalkar@ehrdata.com>
2021-12-11 20:31:36 +0530

    removed ununsed assets

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header__left">ANURAG ARWALAR PORTFOLIO</div>
+      <div className="header__right">
+        <div className="header__skill">ABOUT</div>
+        <div className="header__skill">SKILLS</div>
+        <div className="header__skill">PORTFOLIO</div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
